6abb964282776251225777ec9bcdab6f792ca541
Alexis Villaraza <alvillaraza@Alexiss-MacBook-Air.local>
2021-03-18 19:29:01 -0700

    able to access db.json api

--- vue-crash-course/src/App.vue
+++ vue-crash-course/src/App.vue
@@ -55,7 +55,14 @@ export default {
       );
     },
     async fetchTasks() {
-      const res = await fetch("http://localhost:5000/tasks");
+      const res = await fetch("api/tasks");
+
+      const data = await res.json();
+
+      return data;
+    },
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks/${id}`);
 
       const data = await res.json();
 
