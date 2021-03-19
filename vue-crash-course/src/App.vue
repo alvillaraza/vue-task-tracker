7428536c49db09ab6f16dc6500fda3e182d05e19
Alexis Villaraza <alvillaraza@Alexiss-MacBook-Air.local>
2021-03-18 18:59:58 -0700

    toggles the add task button

--- vue-crash-course/src/App.vue
+++ vue-crash-course/src/App.vue
@@ -1,36 +1,55 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker" />
-    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
+    <Header @toggle-add-task="toggleAddTask" title="Task Tracker" />
+    <div v-show="showAddTask">
+    <AddTask @add-task="addTask" />
+  </div>
+    <Tasks
+      @toggle-reminder="toggleReminder"
+      @delete-task="deleteTask"
+      :tasks="tasks"
+    />
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
 
 export default {
   name: "App",
   components: {
     Header,
     Tasks,
+    AddTask,
   },
   data() {
     return {
       tasks: [],
+      showAddTask: false
     };
   },
   methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask
+    },
+    // the "task" being passed in is from newTask in AddTask.vue
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
     deleteTask(id) {
       if (confirm("Are you sure?")) {
         // filter grabs every task except for the id of the task you're deleting, so it returns non-deleted tasks
         this.tasks = this.tasks.filter((task) => task.id !== id);
       }
     },
-    toggleReminder(id){
-      // does the task equal the id that was passed in? if so, turn reminder into the opposite of what it was. otherwise, just return the task 
-      this.tasks = this.tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task)
-}
+    toggleReminder(id) {
+      // does the task equal the id that was passed in? if so, turn reminder into the opposite of what it was. otherwise, just return the task
+      this.tasks = this.tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      );
+    },
   },
   created() {
     this.tasks = [
